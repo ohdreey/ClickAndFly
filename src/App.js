625113cbdeeae8341303fe2ee549f6983315d050
Lyndia Laurole <lyndia.laurole@gmail.com>
2022-01-07 09:51:45 +0100

    finish trello first run

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import logo from './logo.svg'
 import './App.css'
+
+import { Routes, Route } from 'react-router-dom'
 import Home from './screens/Home'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. dasdasdasd dasdassd
-          dasdasd dasdasdasdfddasdass sadasda njhjhkh dfsdf dasda czz
-          sadasdsssdasdasd dasdassss
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React pdsdasdc dsfada sda
-        </a>
-      </header>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        
+      </Routes>
     </div>
   )
 }
 
 export default App
+
---
+++ src/components/Articlebox.js
@@ -0,0 +1,34 @@
+import './Articlebox.css'
+import Article from './Article'
+
+
+const articleItems = [
+  {
+      categorie: "Le Titre Principale",
+      itemName: "Le Titre",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare nec eros sit amet sollicitudin. In faucibus nunc lobortis, dictum libero sed, mattis felis. Nunc pharetra ac justo non auctor. Cras suscipit consequat orci, rhoncus posuere leo sodales nec. Mauris neque nisl, fringilla nec urna imperdiet, vulputate pellentesque ante. Maecenas blandit pulvinar turpis, ac faucibus diam vulputate sit amet. Nunc euismod ligula placerat, pharetra tellus sit amet, suscipit odio. Vivamus placerat accumsan volutpat. Cras nisi justo, maximus quis velit ut, pellentesque auctor tellus.",
+  }
+]
+const Articlebox = () => {
+  return (
+    <div>
+      
+      {articleItems.map((items) =>(
+      <div>
+        <div className='cateBox'><h1>{items.categorie}</h1></div>
+          <div className='artBox'>
+          <Article
+            title={items.itemName}
+            description={items.description} />
+          <Article
+            title={items.itemName}
+            description={items.description} />
+          <Article
+            title={items.itemName}
+            description={items.description} />
+        </div></div>
+      ))}
+    </div>
+    )}
+
+export default Articlebox
