c981778c91c12f1c51271c66be331a548578ab8a
Lyndia Laurole <lyndia.laurole@gmail.com>
2022-01-27 09:44:46 +0100

    réglages api nearly done, revoir css sous chapitres

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,7 +3,7 @@ import './Recipe.css'
 import { useParams  } from 'react-router-dom'
 
 
-function Recipe ({selectPays,SetSelectPays}) {
+function Recipe () {
     const [datas, setDatas] = useState([])
     
     let params = useParams();
@@ -12,35 +12,33 @@ useEffect(() => {
         fetch(`https://raw.githubusercontent.com/JulesCharles/API-PAYS/master/api/id/${params.id}.json`)
         .then((res) => res.json())
         .then((res) => setDatas(res))
-}, [])
+}, [params.id])
 
 
 
     return(
         <div className='artcont'>
-            
                 <div className='cateBox'>
-                    <h1>Recettes</h1>
+                    <h3>Spécialités Culinaires</h3>
                 </div>
                 <div className='artBox'>
                     <div className='article'>
                         <div>
-                            <img src={datas.food01} alt='' />
+                            <img className='imageR'src={datas.food01} alt={datas.foodName01} />
                         </div>
                         <div>
-                            <h2>{datas.foodName01}</h2>
+                            <h4>{datas.foodName01}</h4>
                             <p>{datas.recipe01}</p>
                         </div>
                     </div>
                 </div>
-
                 <div className='artBox'>        
                     <div className='article'>
                         <div>
-                            <img src={datas.food02} alt='' />
+                            <img className='imageR' src={datas.food02} alt='' />
                         </div>
                         <div>
-                            <h2>{datas.foodName02}</h2>
+                            <h4>{datas.foodName02}</h4>
                             <p>{datas.recipe02}</p>
                         </div>
                     </div>
@@ -48,10 +46,10 @@ useEffect(() => {
                 <div className='artBox'>
                     <div className='article'>
                         <div>
-                            <img src={datas.food03} alt='' />
+                            <img className='imageR' src={datas.food03} alt='' />
                         </div>
                         <div>
-                            <h2>{datas.foodName03}</h2>
+                            <h4>{datas.foodName03}</h4>
                             <p>{datas.recipe03}</p>
                         </div>
                     </div>
